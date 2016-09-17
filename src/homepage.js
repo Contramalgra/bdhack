878778e522bad78ceb8d0596edbee4a043bf0a63
Derek Li <derek.li@alum.utoronto.ca>
2016-09-17 15:52:26 -0700

    basic topic category filter

--- src/homepage.js
+++ src/homepage.js
@@ -1,37 +1,69 @@
 import React from 'react';
 import http from 'superagent';
 
+const categories = [
+  {name: 'Kitchen', value: 'kitchen'},
+  {name: 'Bathroom', value: 'bathroom'},
+  {name: 'Outdoors', value: 'outdoors'}
+];
+
 class MainTable extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-       header: "Header from state...",
-       content: "Content from state...",
-       topics: []
+       topics: [],
+       filteredTopics: [],
+       filterCategory: ''
     }
+    this.filter = this.filter.bind(this);
   }
 
   componentDidMount() {
     http.get('/topics')
       .end((err, res) => {
-        this.setState({topics: res.body});
+        this.setState({topics: res.body, filteredTopics: res.body});
       });
+  }
+
+  filter(e) {
+    const value = e.target.value;
+    let filtered;
+    if (!value) {
+      filtered = this.state.topics;
+    }
+    else {
+      filtered = this.state.topics.filter(topic => topic.category === value);
+    }
+    this.setState({filterCategory: value, filteredTopics: filtered});
   }
 	
 	render() {
 		return (
-			<table>
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Budget</th>
-					</tr>
-				</thead>
-				<tbody>
-					{this.state.topics.map(topic => <tr key={topic._id}><td>{topic.title}</td><td>{topic.budget}</td></tr>)}
-				</tbody>
-			</table>
+      <div>
+        <select onChange={this.filter} value={this.state.filterCategory}>
+          <option value="">All</option>
+          {categories.map(category => <option key={category.value} value={category.value}>{category.name}</option>)}
+        </select>
+  			<table>
+  				<thead>
+  					<tr>
+  						<th>Title</th>
+              <th>Category</th>
+  						<th>Budget</th>
+  					</tr>
+  				</thead>
+  				<tbody>
+  					{this.state.filteredTopics.map(topic => 
+              <tr key={topic._id}>
+                <td>{topic.title}</td>
+                <td>{topic.category}</td>
+                <td>{topic.budget}</td>
+              </tr>
+            )}
+  				</tbody>
+  			</table>
+      </div>
 		);
 	}
 }
