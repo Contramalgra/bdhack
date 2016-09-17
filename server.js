6d435781a27d7342ff99d030f051912d3a27f0b8
Derek Li <derekli@Dereks-MacBook.local>
2016-09-17 11:44:04 -0700

    proxy to BD api

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const proxy = require('express-http-proxy');
 const config = require('./config.json');
 
 const app = express();
 
 app.use('/build', express.static('build'));
+
+// Proxy requests to BuildDirect API
+app.use('/api', proxy('https://api.builddirect.io', {
+  decorateRequest: (proxyReq, originalReq) => {
+    proxyReq.headers['Ocp-Apim-Subscription-Key'] = config.apiKey;
+  }
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
