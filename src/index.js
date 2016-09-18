b515485f4e68910e5d724ad7b526b59192a888d6
Derek Li <derek.li@alum.utoronto.ca>
2016-09-17 20:57:25 -0700

    resolved conflicts and merged

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { Router, Route, browserHistory } from 'react-router';
 
 import HomePage from './homepage';
 import PostTopic from './postTopic';
-import BdProductInfo from './bdProductInfo';
+import Post from './post';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={HomePage} />
     <Route path="/new-topic" component={PostTopic} />
-    {/*<Route path="/topics/:id/details" component={} />*/}
+    <Route path="/topics/:id/details" component={Post} />
   </Router>
 ), document.getElementById('app'));
